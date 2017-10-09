837131c875b9884f36fa88fc4e5e59000d6dd97e
Gaurav <gaurav.purohit024@gmail.com>
2017-10-09 18:00:54 +0530

    Updated

---
+++ CoreDataApp/AddViewController.swift
@@ -0,0 +1,103 @@
+//
+//  AddViewController.swift
+//  CoreDataApp
+//
+//  Created by Gaurav on 14/09/17.
+//  Copyright © 2017 Gaurav. All rights reserved.
+//
+
+import UIKit
+import  CoreData
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var txtcityname: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func tapped(_ sender: UIButton) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.undoManager = UndoManager()
+        context.undoManager?.levelsOfUndo = 3
+        
+        
+        
+        
+        let task = Task(context: context)
+        task.cityActive=true
+        task.cityName=txtcityname.text
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        print("before")
+        getRecords()
+        
+        context.undo()
+         (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        print("after undo")
+         getRecords()
+        
+        context.redo()
+         (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        print("after redo")
+      getRecords()
+
+     navigationController?.popViewController(animated: true)
+
+    }
+    
+    func getRecords() {
+        
+         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do{
+            let data = try context.fetch(Task.fetchRequest())
+            print(data)
+            
+        } catch {
+            
+        }
+    }
+
+    @IBAction func searchpreassed(_ sender: UIButton) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let fetchdata = NSFetchRequest<NSFetchRequestResult>(entityName: "Task")
+        
+        do {
+      
+           // fetchdata.predicate = NSPredicate(format: "cityName == %@",txtcityname.text!)
+             fetchdata.predicate = NSPredicate(format: "cityName contains[c] %@",txtcityname.text!)
+            let data = try context.fetch(fetchdata) as! [Task]
+           
+
+            print(data)
+            
+        } catch {
+            
+        }
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CoreDataApp/ViewController.swift
+++ CoreDataApp/ViewController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+    var tasks = [Task]()
+    
+    @IBOutlet weak var tableList: UITableView!
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        getData()
+        tableList.reloadData()
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +31,58 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func getData()  {
+        
+         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do {
+            let fetchr = NSFetchRequest<NSFetchRequestResult>(entityName: "Task")
+            let sort = NSSortDescriptor(key: "cityName", ascending: false)
+            fetchr.sortDescriptors = [sort]
+            
+            tasks = try context.fetch(fetchr) as! [Task]
+            
+        } catch {
+            print("fetching failed")
+        }
+    }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let tk = tasks[indexPath.row]
+        cell.textLabel?.text = tk.cityName
+        
+        return cell
+    }
+    
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let task = tasks[indexPath.row]
+            context.delete(task)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            do {
+                tasks = try context.fetch(Task.fetchRequest())
+            } catch {
+                print("fetching failed")
+            }
+            
+            tableView.reloadData()
+            
+            
+        }
+    }
 }
 
